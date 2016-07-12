1e13b79f187a8c69c1098930645230a74cd9c460
Nikola Tosic <tosicnikola10@gmail.com>
2016-07-13 08:33:48 +1000

    Setting correct version of qunit (rather than latest). Updating tests

--- js/jquery.imageslideshow.tests.js
+++ js/jquery.imageslideshow.tests.js
@@ -5,26 +5,26 @@ var arrImageList =['http://www.westminsterkennelclub.org/breedinformation/workin
 $module;
 
 test("initAndConfiguration",function(){
-      
-    var expectedCssProp = "url(" + arrImageList[0] + ")",transitionTestCounter =1,
+
+    var expectedCssProp = "url(\"" + arrImageList[0] + "\")",transitionTestCounter =1,
     animationTime = 300,
     transitionTime = 2000;
-      
+
     $module = $('<div></div>').imageSlideshow({
         imageList : arrImageList,
         animationTime : animationTime,
         transitionTime : transitionTime,
         loop: false
     });
-     
+
     equal($module.find('.current-img').css('background-image'),expectedCssProp,"First background image upon init OK");
     stop();
     var testTransition = function(){
         //notify qunit we're to starting test so test context can be initalized
         start();
-        expectedCssProp = "url(" + arrImageList[transitionTestCounter++] + ")";            
+        expectedCssProp = "url(\"" + arrImageList[transitionTestCounter++] + "\")";
         equal($module.find('.current-img').css('background-image'),expectedCssProp,(transitionTestCounter-1) + " transition OK");
-                    
+
         if(transitionTestCounter < arrImageList.length){
             //notify qunit test is finished
             stop();
@@ -34,26 +34,26 @@ test("initAndConfiguration",function(){
             stop();
             setTimeout(function(){
                 start();
-                            
+
                 //after last transition and loop set to false, we're expected to see last image in array as image shown
-                expectedCssProp = "url(" + arrImageList[arrImageList.length - 1] + ")";     
-                equal($module.find('.current-img').css('background-image'),expectedCssProp,"Loop false OK");                 
-                                                    
+                expectedCssProp = "url(\"" + arrImageList[arrImageList.length - 1] + "\")";
+                equal($module.find('.current-img').css('background-image'),expectedCssProp,"Loop false OK");
+
             },transitionTime + 20);
         }
     };
-    setTimeout(testTransition,animationTime + transitionTime + 20);               
-}); 
-            
-          
-          
+    setTimeout(testTransition,animationTime + transitionTime + 20);
+});
+
+
+
 test('moveAndEvents',function(){
     var eventFired = false,
     eventHandler = function(){
         eventFired = true;
     },
-    expectedCssProp = "url(" + arrImageList[2] + ")"
-            
+    expectedCssProp = "url(\"" + arrImageList[2] + "\")"
+
     $module = $('<div></div>').imageSlideshow({
         imageList : arrImageList,
         animationTime : 300,
@@ -61,16 +61,14 @@ test('moveAndEvents',function(){
         loop: false,
         autoPlay: false
     });
-     
+
     $module.bind('imageChanged',eventHandler);
-                
+
     //display third image
     $module.imageSlideshow('displayImage',2);
-                
+
     //test if third image shown
     equal($module.find('.current-img').css('background-image'),expectedCssProp,"displayImgae method OK");
-                
-    ok(eventFired, "imageChanged event fired OK");               
+
+    ok(eventFired, "imageChanged event fired OK");
 });
-
-
